80db21332a01ecceb3dee7c8f55d07da8aa2903c
nshukla75 <nitushukla@yahoo.com>
2015-10-01 05:46:37 -0700

    First Commit

---
+++ app/src/main/java/com/example/nitus/popularmovies/MainActivityFragment.java
@@ -0,0 +1,236 @@
+package com.example.nitus.popularmovies;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+    String[] movieId, movieTitle, movieOverview,movieReleaseDate, moviePosterPath, movieVoteAverage;
+    MovieAdapter mMovieAdapter;
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+   @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // TODO Add your menu entries here
+        inflater.inflate(R.menu.moviefragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_popularity) {
+           updateMovie("popularity.desc");
+            return true;
+        }
+        if (id == R.id.action_rating) {
+            updateMovie("vote_average.desc");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mMovieAdapter = new MovieAdapter(getActivity());
+        GridView listView = (GridView) rootView.findViewById(R.id.gridview_movie);
+        listView.setAdapter(mMovieAdapter);
+        updateMovie("popularity.desc");
+        return rootView;
+    }
+
+    private void updateMovie(String sortBy) {
+        FetchMovieTask movieTask = new FetchMovieTask();
+        movieTask.execute(sortBy);
+    }
+
+
+    public class FetchMovieTask extends AsyncTask<String, Void,  List<String>> {
+        private final String LOG_TAG = FetchMovieTask.class.getSimpleName();
+
+        @Override
+        protected List<String> doInBackground(String... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            // Will contain the raw JSON response as a string.
+            // Will contain the raw JSON response as a string.
+            String movieJsonStr = null;
+            String apiKeyStr = "7537b743615a000671a98c32d354df39";
+
+            try {
+                final String FORECAST_BASE_URL = "http://api.themoviedb.org/3/discover/movie?";
+                final String QUERY_PARAM = "sort_by";
+                final String APIKEY_PARAM = "api_key";
+
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(APIKEY_PARAM, apiKeyStr)
+                        .build();
+                URL url = new URL(builtUri.toString());
+
+                //URL url = new URL("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7537b743615a000671a98c32d354df39");
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                movieJsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            try {
+                return getMovieDataFromJson(movieJsonStr);
+            } catch (JSONException j) {
+                Log.e(LOG_TAG, "JSON Error", j);
+            }
+            return null;
+        }
+
+        private List<String> getMovieDataFromJson(String movieJsonStr)
+                throws JSONException {
+            JSONObject movieJson = new JSONObject(movieJsonStr);
+            JSONArray movieArray = movieJson.getJSONArray("results");
+            List<String> urls = new ArrayList<>();
+            for (int i = 0; i < movieArray.length(); i++) {
+                JSONObject movie = movieArray.getJSONObject(i);
+                urls.add("http://image.tmdb.org/t/p/w185" + movie.getString("poster_path"));
+            }
+            return urls;
+        }
+
+        @Override
+        protected void onPostExecute(List<String> result) {
+                mMovieAdapter.replace(result);
+            }
+        }
+
+
+    class MovieAdapter extends BaseAdapter {
+        private final String LOG_TAG = MovieAdapter.class.getSimpleName();
+        private final Context context;
+        private final List<String> urls = new ArrayList<String>();
+
+        public MovieAdapter(Context context) {
+            this.context = context;
+            //Collections.addAll(urls, moviePosterPath);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = new ImageView(context);
+            }
+            ImageView imageView = (ImageView) convertView;
+
+
+            String url = getItem(position);
+
+            Log.e(LOG_TAG," URL "+url);
+
+            Picasso.with(context).load(url).into(imageView);
+
+            return convertView;
+        }
+
+        @Override
+        public int getCount() {
+            return urls.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return urls.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+        public void replace(List<String> urls) {
+            this.urls.clear();
+            this.urls.addAll(urls);
+            notifyDataSetChanged();
+        }
+    }
+
+}
