82283b7ebb07428bb9308f49e12663c379d16d0f
nshukla75 <nitushukla@yahoo.com>
2015-10-06 23:12:23 -0700

    Updated to new API 23

---
+++ app/src/main/java/com/example/nitu/popularmovies/DetailActivityFragment.java
@@ -0,0 +1,52 @@
+package com.example.nitu.popularmovies;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailActivityFragment extends Fragment {
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    public DetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //return inflater.inflate(R.layout.fragment_detail, container, false);
+        Intent intent = getActivity().getIntent();
+        View rootView =  inflater.inflate(R.layout.fragment_detail, container, false);
+        if (intent != null && intent.hasExtra(intent.EXTRA_TEXT))
+        {
+            MovieData movie = (MovieData)intent.getSerializableExtra(intent.EXTRA_TEXT);
+            String mMovieTitle = movie.getTitle();
+            ((TextView)rootView.findViewById(R.id.title_text))
+                    .setText(mMovieTitle);
+            String url =movie.getPoster_path();
+            Picasso.with(getActivity()).load(url).into((ImageView) rootView.findViewById(R.id.imageView));
+            String mMovieVoteCount = " ("+ movie.getVote_count()+" Votes)";
+            ((TextView)rootView.findViewById(R.id.votecount_text))
+                    .setText(mMovieVoteCount);
+            String mMovieVoteAverage = movie.getVote_average();
+            ((TextView)rootView.findViewById(R.id.voteaverage_text))
+                    .setText(mMovieVoteAverage);
+            String mMovieReleaseDate = movie.getRelease_date();
+            ((TextView)rootView.findViewById(R.id.release_text))
+                    .setText(mMovieReleaseDate);
+            String mMovieOverview = movie.getOverview();
+            ((TextView)rootView.findViewById(R.id.overview_text))
+                    .setText(mMovieOverview);
+
+        }
+        return rootView;
+    }
+}
