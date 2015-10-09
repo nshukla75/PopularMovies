9b4b47326999c32a7aad7f07677e21a265f7d0ca
nshukla75 <nitushukla@yahoo.com>
2015-10-08 22:04:00 -0700

    main and setting done

--- app/src/main/java/com/example/nitu/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/example/nitu/popularmovies/SettingsActivity.java
@@ -144,23 +144,11 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_data_sync);
-            //setHasOptionsMenu(true);
-
             // Bind the summaries of EditText/List/Dialog/Ringtone preferences
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
             bindPreferenceSummaryToValue(findPreference("sort_order"));
         }
-
-       /* @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }*/
     }
 }
